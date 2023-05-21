930b2671c2eed33cb20ca15c2677cb8097a05783
juandapps <juandapps@hotmail.com>
2023-05-20 21:05:14 -0500

    ➕Add post show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
+  # Define the root path route
   root 'blog_posts#index'
 end
